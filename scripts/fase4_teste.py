04ac960216e61e766d0989482fc4dfa1702318e1
Miguel Aguena <miguel.aguena@usp.br>
2022-08-26 16:33:09 -0300

    Finishing Implementation of Trajectory

--- scripts/fase4_teste.py
+++ scripts/fase4_teste.py
@@ -1,28 +1,23 @@
 #!/usr/bin/env python3
-import cv2
-import rclpy
 import numpy as np
 from pickle import FALSE
 import time
-sys.path.insert(0,'/home/' + os.environ["USER"] + '/skyrats_ws2/src/mavbase2')
-from MAV2 import MAV2
-
-from tf_transformations import quaternion_from_euler, euler_from_quaternion
 
 TOL = 0.5 #tolerancia de erro das bases 
 CAM_FOV = 1.64061 #94 graus
+coords = [0, 0, 0]
 
 class CoastBase:
     def __init__(self, mav):
         self.mav = mav
-        self._coordinates = [self.mav.drone_pose.pose.position.x, self.mav.drone_pose.pose.position.y, self.mav.drone_pose.pose.position.z]
+        self._coordinates = [0, 0, 0]
 
 class Base:
-    def __init__(self, name, mav, x, y, z):
+    def __init__(self, name, mav, x, y, z, package):
         self.mav = mav
         self._name = name
         self._coordinates = [x, y, z]
-        self._package = None
+        self._package = package
 
     def hasCorrectPackage(self):
         if(self._package == self._name):
@@ -31,9 +26,11 @@ class Base:
             return False
 
     def getDistanceToDrone(self):
-        deltax = self._coordinates[0] - self.mav.drone_pose.pose.position.x
-        deltay = self._coordinates[1] - self.mav.drone_pose.pose.position.y
-        deltaz = self._coordinates[2] - self.mav.drone_pose.pose.position.z
+        global coords
+
+        deltax = self._coordinates[0] - coords[0]
+        deltay = self._coordinates[1] - coords[1]
+        deltaz = self._coordinates[2] - coords[2]
 
         return np.sqrt(np.power(deltax, 2) + np.power(deltay, 2) + np.power(deltaz, 2))
 
@@ -41,7 +38,7 @@ class fase4:
     def __init__(self, mav2):
         self.mav2 = mav2
         self.coastBase = CoastBase(self.mav2)
-        self.bases = [Base("A", self.mav2, 0, 0, 0), Base("B", self.mav2, 0, 0, 0), Base("C", self.mav2, 0, 0, 0), Base("D", self.mav2, 0, 0, 0), Base("E", self.mav2, 0, 0, 0)] #Como as bases serão transmitidas da fase1 para a fase4?
+        self.bases = [Base("A", self.mav2, 0, 1, 0, "D"), Base("B", self.mav2, 2, 3, 1, "E"), Base("C", self.mav2, 6, 7, 0, "A"), Base("D", self.mav2, 8, 0, 8, "C"), Base("E", self.mav2, 4, 4, 4, "B")] #Como as bases serão transmitidas da fase1 para a fase4?
         self.pickedPackage = None
         self.destination = None
 
@@ -61,14 +58,19 @@ class fase4:
 
         return nearestBase
 
-    def deliverQrcode(self, mask_tube): 
+    def deliverQrcode(self):
+        print("deliverQrcode")
 
-    def pickQrcode(self, mask_tube): 
-    
+    def pickQrcode(self): 
+        print("pickQrcode")
+
     def findQrcode(self):
+        print("findQrcode")
        
     def trajectory(self):
-        self.mav2.takeoff(3)
+        global coords
+
+        print("Start!")
 
         nearestBase = self.getNearestBaseWithoutCorrectPackage()
 
@@ -76,20 +78,29 @@ class fase4:
             self.destination = nearestBase
 
             while(not self.destination == None):
-                self.mav2.go_to_local(self.destination._coordinates[0], self.destination._coordinates[1], self.destination._coordinates[2])
+                print("Next destination: " + self.destination._name)
+
+                coords[0] = self.destination._coordinates[0]
+                coords[1] = self.destination._coordinates[1]
+                coords[2] = self.destination._coordinates[2]
 
                 self.findQrcode()
                 self.pickQrcode() #Colocar a máscara
-                foundPackage = #Obter o pacote
+                foundPackage = self.destination._package #Obter o pacote
 
                 if(not foundPackage == None):
+                    print("Found " + foundPackage)
+
                     if(not self.pickedPackage == None):
+                        print("Delivering " + self.pickedPackage)
                         self.deliverQrcode()
                         for i in range(len(self.bases)):
                             if (self.destination._name == self.bases[i]._name):
                                 self.bases[i]._package = self.pickedPackage
+                                print(self.bases[i]._name + " -> " + self.bases[i]._package)
                                 
                         self.pickedPackage = foundPackage
+                        print("Picking " + self.pickedPackage)
                     else:
                         self.pickedPackage = foundPackage
                         for i in range(len(self.bases)):
@@ -102,10 +113,12 @@ class fase4:
 
                 else:
                     if(not self.pickedPackage == None):
+                        print("Delivering " + self.pickedPackage)
                         self.deliverQrcode()
                         for i in range(len(self.bases)):
                             if (self.destination._name == self.bases[i]._name):
                                 self.bases[i]._package = self.pickedPackage
+                                print(self.bases[i]._name + " -> " + self.bases[i]._package)
 
                         self.pickedPackage = None
                         
@@ -114,11 +127,16 @@ class fase4:
             nearestBase = self.getNearestBaseWithoutCorrectPackage()
 
         print("Finished!")
-        self.mav2.go_to_local(self.coastBase._coordinates[0], self.coastBase._coordinates[1], self.coastBase._coordinates[2])
+
+        for i in range(len(self.bases)):
+            print(self.bases[i]._name + " -> " + self.bases[i]._package)
+
+        coords[0] = self.coastBase._coordinates[0]
+        coords[1] = self.coastBase._coordinates[1]       
+        coords[2] = self.coastBase._coordinates[2]
 
 if __name__ == "__main__":
-    rclpy.init()
-    mav = MAV2()
+    mav = None
     missao = fase4(mav)
     missao.trajectory()
     

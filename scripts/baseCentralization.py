12b7f085b4280427f8b633e9d10a0acba5a5cc67
Alessandro Menegon <95109891+minig0n@users.noreply.github.com>
2022-10-13 15:48:10 -0300

    :monkey: CBR base centralization

---
+++ scripts/baseCentralization.py
@@ -0,0 +1,67 @@
+import rclpy
+from baseDetector import CrossDetection
+
+
+def centralize_on_cross(drone):
+
+    '''
+    This function, when started, tries to centralize the drone on a cross base marker.
+    If no markers are detected, the drone is shaked to try a different angle of view.
+    Without detection for a long period, the function is ended.
+
+    Function parameters:
+    drone -> MAV2 object
+
+    '''
+
+    detection = CrossDetection()
+
+    is_centralized = False
+    while not is_centralized:
+
+        self.TARGET = (int(drone.cam.shape[0]/2), int(drone.cam.shape[1]/2))
+        
+        # Loop over frames to search for markers
+        # If no markers were found, tries to shake the drone
+        cross_detected = False
+        timer = 0
+        no_detection = 0
+        
+        while not cross_detected:
+            rclpy.spin_once(drone)
+            
+            frame = drone.cam
+            list_of_bases = detection.base_detection(frame)
+
+            if len(list_of_bases) > 0:
+                cross_detected = True
+                timer = 0
+
+            if timer > 1000:
+                print("No visible bases, shaking drone...")
+                drone.shake()
+                timer = 0
+                no_detection += 1
+
+            if no_detection > 10:
+                print("Aruco not found...")
+                return
+
+            timer += 1
+
+        base = list_of_bases[0]
+
+        # Calculate the PID errors
+        delta_x = self.TARGET[0] - base[0]
+        delta_y = self.TARGET[1] - base[1]
+
+        # Adjust velocity
+        drone.camera_pid(delta_x, delta_y)
+
+        # End centralization if the marker is close enough to the camera center
+        if ((delta_x)**2 + (delta_y)**2)**0.5 < 30:
+            drone.set_vel(0, 0, 0)
+            is_centralized = True
+            print(f"Centralized! x: {delta_x} y: {delta_y}")
+                
+        return

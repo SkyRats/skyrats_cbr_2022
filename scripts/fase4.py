9be76b3659a6f78eae830e5302b20b3736f9e25e
sophi011 <sophiaceline@usp.br>
2022-10-15 22:45:14 +0200

    trajetoria e detecção funcionando em simulação

--- scripts/fase4.py
+++ scripts/fase4.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 import cv2
 import numpy as np
-from mavbase2.MAV2 import MAV2
+#from mavbase2.MAV2 import MAV2
+import sys
+import os
+sys.path.insert(0,'/home/' + os.environ["USER"] + '/skyrats_ws2/src/mavbase2')
+from MAV2 import MAV2
 import time
+import rclpy
+from rclpy.clock import Clock
+from qrDetection import QRDetection
 
 TOL = 0.5 #tolerancia de erro das bases 
 CAM_FOV = 1.64061 #94 graus
@@ -14,23 +21,66 @@ class Base:
         self._coordinates = [x, y, z]
         self._package = None
 
-    def getDistanceToDrone(self):
-        deltax = self._coordinates[0] - self.mav.drone_pose.pose.position.x
-        deltay = self._coordinates[1] - self.mav.drone_pose.pose.position.y
-        deltaz = self._coordinates[2] - self.mav.drone_pose.pose.position.z
-
-        return np.sqrt(np.power(deltax, 2) + np.power(deltay, 2) + np.power(deltaz, 2))
-
 class fase4:
     def __init__(self, mav2):
+        super().__init__()
         self.mav2 = mav2
         self.coastbase_coords = []
-        self.bases = [Base("A", self.mav2, 0, 0, 0), Base("B", self.mav2, 0, 0, 0), Base("C", self.mav2, 0, 0, 0), Base("D", self.mav2, 0, 0, 0), Base("E", self.mav2, 0, 0, 0)] #Como as bases serão transmitidas da fase1 para a fase4?
+        self.bases_not_visited = []
+        self.bases_visited = []
         self.destination = None
+        self.detection = QRDetection()
+        self.detection.frame = mav2.cam
+        self.cam_id = 0
 
+    def trajectory(self):
+
+        rclpy.spin_once(self.mav2) 
+        self.mav2.takeoff(8)
+        current_x = self.mav2.drone_pose.pose.position.x
+        current_y = self.mav2.drone_pose.pose.position.y
+        dist_menor=8*(2**(1/2))
+        qtdade_bases_visited = 0
+
+        self.bases_not_visited.append([-2.5, 0, 1])
+        self.bases_not_visited.append([-4, 2, 0])
+        self.bases_not_visited.append([-6, 0, 0])
+        self.bases_not_visited.append([-2, 5, 0])
+        self.bases_not_visited.append([0.31, 6.1, 1.55])
+
+        while(qtdade_bases_visited!=5):
+            rclpy.spin_once(self.mav2) 
+            current_x = self.mav2.drone_pose.pose.position.x
+            current_y = self.mav2.drone_pose.pose.position.y
+            for i in range(len(self.bases_not_visited)):
+                
+                if self.bases_not_visited[i] not in self.bases_visited:
+                  
+                    X=self.bases_not_visited[i][0]
+                    Y=self.bases_not_visited[i][1]
+
+                    dist=np.sqrt((X- current_x)**2 + (Y - current_y)**2)
+
+                    if dist<dist_menor:
+                        dist_menor=dist
+                        i_oficial=i
+            dist_menor = 8*(2**(1/2))
+            self.mav2.go_to_local(self.mav2.drone_pose.pose.position.x, self.mav2.drone_pose.pose.position.y, 3)
+            self.mav2.go_to_local(self.bases_not_visited[i_oficial][0],self.bases_not_visited[i_oficial][1], 3)
+            self.mav2.go_to_local(self.bases_not_visited[i_oficial][0],self.bases_not_visited[i_oficial][1], self.bases_not_visited[i_oficial][2] + 0.5)
+            qr_result = self.detection.qrdetection(self.mav2.cam)
+            self.mav2.get_logger().warn(str(qr_result))
+            self.bases_visited.append(self.bases_not_visited[i_oficial]) 
+            qtdade_bases_visited += 1
+        rclpy.spin_once(self.mav2) 
+        current_x = self.mav2.drone_pose.pose.position.x
+        current_y = self.mav2.drone_pose.pose.position.y    
+        self.mav2.go_to_local(current_x, current_y, 3)
+        self.mav2.go_to_local(0, 0, 3)
+        self.mav2.land()
 
 if __name__ == "__main__":
-
+    rclpy.init()
     mav = MAV2()
     missao = fase4(mav)
     missao.trajectory()
--- scripts/qrDetection.py
+++ scripts/qrDetection.py
@@ -7,75 +7,82 @@ from defisheye import Defisheye
 class QRDetection():
 
     def __init__(self):
-        self.detection = False
+        self.detected = False
         self.qr_data = ""
         self.qr_debug = False
-        self.qr_result
-        self.frame = None
+        self.qr_result =None
+        self.cam = None # self.cam = cv2.VideoCapture(cam_id)
+        self.frame = None # self.frame é uma imagem de cv (numpy.ndarray) que será processada nas funções
         self.det_number = 0
-        self.qr_x = 0
-        self.qr_y = 0
-        self.qr_w = 0
-        self.qr_h = 0
-        self.image = None
-
+        self.qrs = []
+        self.fisheye = False
     
     def qrdetection(self, vid):
-            ret, self.frame = vid.read()
+            self.frame = vid
             #camera parameters
             dtype = 'linear'
             format = 'fullframe'
             fov = 140
             pfov = 90
             img_out = f"./images/out/TESTE_{dtype}_{format}_{pfov}_{fov}.jpg"
+            timeout = 5
+            init = time.time()
+            now = time.time()
 
-            while self.detection and self.det_number<=10:
-                ret, self.frame = vid.read()
+            while not self.detected:
+                self.qr_data = ""
+                now = time.time()
+                if(now - init) > timeout:
+                    return "Timeout exceeded, giving up QR detection"
+                    
                 #reducing fish eye effect
-                self.image = Defisheye(self.frame, dtype=dtype, format=format, fov=fov, pfov=pfov)            
-                self.image = self.image.convert(img_out)
-                self.frame = self.image
-
-                if self.qr_data != "":
-                    self.det_number += 1
+                self.frame = Defisheye(self.frame, dtype=dtype, format=format, fov=fov, pfov=pfov)            
+                self.frame = self.frame.convert(img_out)
                 
                 self.qr_result = decode(self.frame)
 
-                if len(qr_result)>0:
-                    print("QR Code being detected")
+                if len(self.qr_result)>0:
+                    self.detected = True
+                    init = time.time()
+                    self.det_number += 1
 
-                    for barcode in qr_result:       # writing qr code info in frame
+                    for barcode in self.qr_result:       # writing qr code info in frame
 
-                        (self.qr_x, self.qr_y, self.qr_w, self.qr_h) = barcode.rect
-                        cv2.rectangle(self.frame, (self.qr_x, self.qr_y), (self.qr_x + self.qr_w, self.qr_y + self.qr_h), (0, 0, 255), 2)
+                        (qr_x, qr_y, qr_w, qr_h) = barcode.rect
+                        cv2.rectangle(self.frame, (qr_x, qr_y), (qr_x + qr_w, qr_y + qr_h), (0, 0, 255), 2)
                         self.qr_data = barcode.data.decode("utf-8")
-                        self.qr_type = barcode.type
-                        print("QR Code info: ", self.qr_data)
-                        cv2.putText(self.frame, str(self.qr_data), (self.qr_x, self.qr_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 4)
+                        if self.qr_data not in self.qrs:
+                            self.qrs.append(self.qr_data)
+                        cv2.putText(self.frame, str(self.qr_data), (qr_x, qr_y - 10), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 4)
 
                 if self.qr_debug:
                     cv2.imshow("Frame", self.frame)
+                    ret, self.frame = self.cam.read()
+                    print("QR data: " + str(self.qr_data))
 
                 if cv2.waitKey(1) & 0xFF == ord("q"):
                     break
 
             # cleanup
             cv2.destroyAllWindows()
-            return self.qr_result # qr_result is a list with qr infos from all readings 
+            return self.qr_data # qr_result is a list with qr infos from all readings 
 
 
     def qrtest(self, cam_id): 
-        cam = cv2.VideoCapture(cam_id)
+        self.cam = cv2.VideoCapture(cam_id)
         # changing camera resolution for better processing
-        cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-        cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-        self.detection = True
+        self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
         self.qr_debug = True
-        self.qrdetection(cam)
-        cam.release()     
+        ret, vid = self.cam.read()
+        self.frame = vid
+        result = self.qrdetection(vid)
+        self.cam.release()    
+        return result
 
 
 if __name__ == "__main__"  :
     det = QRDetection()
-    det.qrtest(4)
+    result = det.qrtest(0)
+    print(str(result))
     

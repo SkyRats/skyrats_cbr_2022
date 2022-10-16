29f46238c091786093563f561a229384dfeb16c8
helena <helenamoyen@usp.br>
2022-10-16 10:34:33 -0300

    identify one or empty

--- scripts/displayDetection.py
+++ scripts/displayDetection.py
@@ -11,10 +11,11 @@ class displayDetection:
     
     def __init__(self):
 
-        self.cap = cv2.VideoCapture('/home/renato/skyrats_ws2/src/skyrats_cbr_2022/images/drone.mp4')
+        self.cap = cv2.VideoCapture(2)
 
         self.squares = []
-        self.gas_percentual_list = []
+        self.gas_percentual_list1 = []
+        self.gas_percentual_list2=[]
         self.zero_adjustment_list = []
         self.reader = easyocr.Reader(['pt'])
         self.gas_percentual_image = []
@@ -60,7 +61,7 @@ class displayDetection:
 
         gray = cv2.cvtColor(self.reshaped_image, cv2.COLOR_BGR2GRAY)
         ret, thresh = cv2.threshold(gray, 140, 255, cv2.CHAIN_APPROX_NONE)
-        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv2.findContours(gray, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
         for contour in contours:
             approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
@@ -92,16 +93,16 @@ class displayDetection:
         
         #crop the top side of the image (gas percentual)
         cropped_image1 = cropped_image[round(cropped_image.shape[0]*0.05):round(cropped_image.shape[0]/2), round(cropped_image.shape[1]*0.08):round(cropped_image.shape[1]*0.63)]
-        #cv2.imshow("crop", cropped_image1)
+        cv2.imshow("crop", cropped_image1)
         #crop the bottom side of the image (gas percentual)
         cropped_image2 = cropped_image[round(cropped_image.shape[0]/2):round(cropped_image.shape[0]*0.95), round(cropped_image.shape[1]*0.07):round(cropped_image.shape[1]*0.63)]
 
         #crop the first character of the gas percentual number
         cropped_image3 = cropped_image1[0:cropped_image1.shape[0], round(cropped_image1.shape[1]*0.06):round(cropped_image1.shape[1]*0.55)]
-
+        cv2.imshow("crop2", cropped_image3)
         #crop the second character of the gas percentual number
         cropped_image4 = cropped_image1[0:cropped_image1.shape[0], round(cropped_image1.shape[1]*0.55):cropped_image1.shape[1]]
-
+        
         #crop the zero adjustment number
         cropped_image5 = cropped_image2[0:cropped_image2.shape[0], round(cropped_image2.shape[1]*0.52):round(cropped_image2.shape[1]*0.99)]
 
@@ -163,11 +164,14 @@ class displayDetection:
     def isEmpty(self, image, tolerance):
 
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
         ret, thresh = cv2.threshold(gray, 140, 255, cv2.CHAIN_APPROX_NONE)
+        # cv2.imshow("thresh", thresh)
 
         total = thresh.size
         zero = total - np.count_nonzero(thresh)
         ratio = zero/total
+        print(ratio)
     
         if ratio > tolerance:
             return True
@@ -179,7 +183,7 @@ class displayDetection:
     def detection_loop(self):
         i = 0
         self.reshape = False
-        while self.cap.isOpened() and i<100:
+        while self.cap.isOpened() and i<200 :
 
             self.squares = []
             success, self.image = self.cap.read()
@@ -198,11 +202,15 @@ class displayDetection:
             # self.image = cv2.resize(self.image, (1280,720))
             kernel = np.ones((3, 3), np.uint8)
             self.image = cv2.dilate(self.image, kernel)
-            self.image = cv2.erode(self.image, kernel)
+            # self.image = cv2.erode(self.image, kernel)
             #canny = cv2.Canny(self.image,100,200)
             #cv2.imshow("canny", canny)
             gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-            ret, thresh = cv2.threshold(gray, 140, 255, cv2.CHAIN_APPROX_NONE)
+            # blurred = cv2.GaussianBlur(gray, (7, 7), 0)
+            thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+            # ret, thresh = cv2.threshold(gray, 140, 255, cv2.CHAIN_APPROX_NONE)
+            cv2.imshow("thresh", thresh)
+
             #cv2.imshow("thresh", thresh)
 
             contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
@@ -224,6 +232,8 @@ class displayDetection:
 
 
                 if(i < 20):
+
+                    
                     
                     #apply the OCR algorithm over the numbers images
                     
@@ -237,15 +247,36 @@ class displayDetection:
                     minus = self.isEmpty(self.minus_image, 0.2)
 
 
-                    if self.gas_percentual[0] and self.gas_percentual[1]:
+                    if self.gas_percentual[0]:
+                        self.gas_percentual_list1.append(self.gas_percentual[0])
+                    
+                    else:
+
+                        oneGasPercentual0 = self.isEmpty(self.gas_percentual_image[0], 0.13)
+                        if len(self.squares)==(i+1) and oneGasPercentual0 :
+                            self.gas_percentual_list1.append(1)
+
+
+                    if self.gas_percentual[1]:
                                                
-                        self.gasPercentual = int(str(self.gas_percentual[0]) + str(self.gas_percentual[1]))
-                        self.gas_percentual_list.append(self.gasPercentual)
+                        # self.gasPercentual = int(str(self.gas_percentual[0]) + str(self.gas_percentual[1]))
+                        self.gas_percentual_list2.append(self.gas_percentual[1])
                         # print("Gas Percentual: " + str(self.gasPercentual) + "%")
                     
-                    if self.zero_adjustment:
+                    # if self.squares and self.gas_percentual[0]==0 and self.gas_percentual_list1==[]:
+                    #     self.gas_percentual_list1.append(1)
 
-                        
+                    # if self.squares and self.gas_percentual[1]==0 and self.gas_percentual_list2==[]:
+                    #     self.gas_percentual_list2.append(1)
+
+                    else:
+                        oneGasPercentual1 = self.isEmpty(self.gas_percentual_image[1], 0.13)
+                        if len(self.squares)==(i+1) and oneGasPercentual1:
+                            self.gas_percentual_list2.append(1)
+                            print("oi")
+                
+
+                    if self.zero_adjustment:
 
                         if one:
                             self.zero_adjustment = int(self.zero_adjustment) + 10
@@ -256,15 +287,35 @@ class displayDetection:
                         self.zero_adjustment_list.append(self.zero_adjustment)
 
                         #print("Zero Adjustment: " +str(self.zero_adjustment) + "%")
+
+                    else: 
+                        oneZeroAdj = self.isEmpty(self.zero_adjustment_image, 0.12)
+                        if len(self.squares)==(i+1) and oneZeroAdj:
+
+                            print("Li nada")
+
+                            self.zero_adjustment =1;
+
+                            if one:
+                                self.zero_adjustment = int(self.zero_adjustment) + 10
+
+                            if minus:
+                                self.zero_adjustment = int(self.zero_adjustment)*-1
+
+                            self.zero_adjustment_list.append(self.zero_adjustment)
+
                         
                     
                     i = i + 1
 
                 else:
 
-                    if(len(self.gas_percentual_list)!=0):
+                    if(len(self.gas_percentual_list1)!=0 and len(self.gas_percentual_list2)!=0):
 
-                        self.gasPercentual=mode(self.gas_percentual_list)
+                        self.gas_percentual[0]=mode(self.gas_percentual_list1)
+                        self.gas_percentual[1]=mode(self.gas_percentual_list2)
+
+                        self.gasPercentual = int(str(self.gas_percentual[0]) + str(self.gas_percentual[1]))
                         print("Gas Percentual: " + str(self.gasPercentual) + "%")
 
                     if(len(self.zero_adjustment_list)!=0):

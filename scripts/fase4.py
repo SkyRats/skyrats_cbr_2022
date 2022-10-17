543fa084d01f5a1003bead5e0d0ef94e69a9da10
sophi011 <sophiaceline@usp.br>
2022-10-17 16:56:30 +0200

    ROS 1 syntax

--- scripts/fase4.py
+++ scripts/fase4.py
@@ -3,12 +3,13 @@ import cv2
 import numpy as np
 import sys
 import os
-sys.path.insert(0,'/home/' + os.environ["USER"] + '/skyrats_ws2/src/mavbase2')
-from MAV2 import MAV2
-#from MAV_ardupilot import MAV2
+#sys.path.insert(0,'/home/' + os.environ["USER"] + '/skyrats_ws2/src/mavbase2')
+#from MAV2 import MAV2
+from MAV_ardupilot import MAV2
 import time
-import rclpy
-from rclpy.clock import Clock
+#import rclpy
+import rospy
+#from rclpy.clock import Clock
 from qrDetection import QRDetection
 
 CAM_FOV = 1.64061 #94 graus
@@ -29,13 +30,12 @@ class fase4:
         self.bases_visited = []
         self.destination = None
         self.detection = QRDetection()
-        self.detection.frame = mav2.cam
+        self.detection.cam = mav2.cam
         self.cam_id = 0
 
     def trajectory(self):
         self.mav2.change_auto_speed(0.8)
         self.detection.qr_debug = False
-        rclpy.spin_once(self.mav2) 
         self.mav2.takeoff(2)
         current_x = self.mav2.drone_pose.pose.position.x
         current_y = self.mav2.drone_pose.pose.position.y
@@ -49,9 +49,7 @@ class fase4:
         self.bases_not_visited.append([0.31, 6.1, 1.55])
 
         while(qtdade_bases_visited!=5):
-            rclpy.spin_once(self.mav2) 
-            current_x = self.mav2.drone_pose.pose.position.x
-            current_y = self.mav2.drone_pose.pose.position.y
+
             for i in range(len(self.bases_not_visited)):
                 
                 if self.bases_not_visited[i] not in self.bases_visited:
@@ -65,7 +63,6 @@ class fase4:
                         dist_menor=dist
                         i_oficial=i
             dist_menor = 8*(2**(1/2))
-            self.mav2.go_to_local(self.mav2.drone_pose.pose.position.x, self.mav2.drone_pose.pose.position.y, 2)
             self.mav2.go_to_local(self.bases_not_visited[i_oficial][0],self.bases_not_visited[i_oficial][1], 2)
             self.mav2.go_to_local(self.bases_not_visited[i_oficial][0],self.bases_not_visited[i_oficial][1], self.bases_not_visited[i_oficial][2] + 0.5)
             qr_result = self.detection.qrdetection(self.mav2.cam)
@@ -73,6 +70,11 @@ class fase4:
             if not self.detection.detected:
                 self.mav2.get_logger().warn("Trying to detect again...")
                 self.mav2.go_to_local(self.mav2.drone_pose.pose.position.x, self.mav2.drone_pose.pose.position.y, self.mav2.drone_pose.pose.position.z + 0.2, 0, 0.1)
+                qr_result = self.detection.qrdetection(self.mav2.cam)
+                self.mav2.get_logger().warn("QR data: " + str(qr_result))
+            if not self.detection.detected:
+                self.mav2.get_logger().warn("Trying to detect again...")
+                self.mav2.go_to_local(self.mav2.drone_pose.pose.position.x, self.mav2.drone_pose.pose.position.y, self.mav2.drone_pose.pose.position.z - 0.5, 0, 0.1)
                 qr_result = self.detection.qrdetection(self.mav2.cam)
                 self.mav2.get_logger().warn("QR data: " + str(qr_result))
             self.detection.detected = False
@@ -89,20 +91,25 @@ class fase4:
     def trajectory_test(self):
         self.mav2.change_auto_speed(0.5)
         self.detection.qr_debug = False
-        rclpy.spin_once(self.mav2) 
-        self.mav2.takeoff(2)
-        current_x = self.mav2.drone_pose.pose.position.x
-        current_y = self.mav2.drone_pose.pose.position.y
+        self.mav2.takeoff(1)
 
-        self.mav2.go_to_local(-1, 1, self.mav2.drone_pose.pose.position.z)
-        qr_result = self.detection.qrdetection(self.mav2.cam)
-        self.mav2.get_logger().warn("QR data: " + str(qr_result))
+        self.mav2.go_to_local(1, 1, 1)
+        self.mav2.go_to_local(1, 1, 0.5)
+        cam = cv2.VideoCapture(0)
+        qr_result = self.detection.qrdetection(cam)
+        rospy.logwarn("QR data: " + str(qr_result))
         if not self.detection.detected:
-            self.mav2.get_logger().warn("Trying to detect again...")
-            self.mav2.go_to_local(self.mav2.drone_pose.pose.position.x, self.mav2.drone_pose.pose.position.y, self.mav2.drone_pose.pose.position.z + 0.2, 0, 0.1)
-            qr_result = self.detection.qrdetection(self.mav2.cam)
+            rospy.logwarn("Trying to detect again...")
+            self.mav2.go_to_local(1, 1, 0.7)
+            qr_result = self.detection.qrdetection(cam)
+            rospy.logwarn("QR data: " + str(qr_result))
+        if not self.detection.detected:
+            rospy.logwarn("Trying to detect again...")
+            self.mav2.go_to_local(1, 1, 0.3)
+            qr_result = self.detection.qrdetection(cam)
             self.mav2.get_logger().warn("QR data: " + str(qr_result))
-        self.mav2.go_to_local(0, 0, 2)
+        self.mav2.go_to_local(1, 1, 1)
+        self.mav2.go_to_local(0, 0, 1)
         self.mav2.land()
         self.mav2.get_logger().warn("QRs detected: " + str(self.detection.qrs))
 

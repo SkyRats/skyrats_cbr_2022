edbe688e6220f55824ca7b76d015aa0da2c31d6a
Bárbara_Bueno <65515809+Buenobarbie@users.noreply.github.com>
2022-10-19 21:25:54 -0300

    Detectando medidas do tubo

--- scripts/tuboDetect.py
+++ scripts/tuboDetect.py
@@ -1,23 +1,20 @@
-from xml.dom import NotFoundErr
 import cv2
 import numpy as np
 from statistics import mode
 TOL = 30
 
  
-#VERIFICAR AREA MINNIMA 
-#VERIFICAR FATOR DE CORREÇÃO
-
 class tuboDetection:
     
     def __init__(self):
         self.capture = cv2.VideoCapture(0)
-        self.redSquaresCount = 0
-        self.orangeSquaresCount = 0
-        self.orangeRecord = [0 for i in range(TOL)]
         self.tuboDetected = False
-        self.fatorDeCorrecaoWidth = 1.29
-        self.fatorDeCorrecaoHeight = 0.714
+        self.fatorDeCorrecaoWidth =  0.9487
+        self.fatorDeCorrecaoHeight = 0.6834
+
+        self.drone_x = -278
+        self.drone_y = 213
+        
         
         
     def get_mask(self, hsv , lower_color , upper_color):
@@ -26,32 +23,55 @@ class tuboDetection:
         mask = cv2.inRange(hsv , lower, upper)
         return mask
 
-    def get_square_area(self,img):
-        squaresDetected = []
-        min_area = 1000 #checar valor mais apropriado
+    def x_conversion(self,x,width):
+        return  self.drone_y + ( x - width/2) * self.fatorDeCorrecaoWidth
     
+    def y_conversion(self,y,height):
+        return  ( -1 * self.drone_x)+ ( y - height/2) * self.fatorDeCorrecaoWidth
+        
+
+    def get_square_area(self,img):
+        min_area = 15000 #checar valor mais apropriado
+        min_ratio = 10
+
+        img_width , img_height = img.shape
+
         contours,junk=cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
         #cv2.drawContours(frame,contours,-1,(255,0,0),3)
         
         for contour in contours:
             area = cv2.contourArea(contour)
+            #print(f"Area: {area}")
         
             if area >= min_area:
-                #cv2.drawContours(frame,[contour],0,(255,0,0),3)
+                cv2.drawContours(img,[contour],0,(255,0,0),3)
                 (x,y), (w,h), theta = cv2.minAreaRect(contour)
 
-                #cv2.rectangle(self.frame,(x,y),(x+w,y+h),(0,0,255),3)
                 if h > w:
                     temp = h
                     h = w
                     w = temp
 
-                w = round(w * self.fatorDeCorrecaoWidth,2)
-                h = round(h * self.fatorDeCorrecaoHeight,2)
-                print(f"Comprimento do tubo : {w} cm")
-                print(f"Largura do tubo : {h } cm")
-                cv2.imwrite("./tuboMask.png", img)
-                self.tuboDetected = True
+                if(w/h) > min_ratio:
+                    w = round(w * self.fatorDeCorrecaoWidth,2)
+                    h = round(h * self.fatorDeCorrecaoHeight,2)
+                    print(f"Comprimento do tubo : {w} cm")
+                    print(f"Largura do tubo : {h } cm")
+
+                    cv2.imwrite("./tuboMask.png", img)
+
+                    rect = ((x, y), (w, h), theta)
+                    box = cv2.boxPoints(rect) 
+                    print("Coordenadas do tubo:")
+
+                    print(f"Point 1  x:{self.x_conversion(img_width - box[0][1],img_width)}  y:{self.y_conversion(img_height - box[0][0], img_height)}")
+                    print(f"Point 2  x:{self.x_conversion(img_width - box[1][1],img_width)}  y:{self.y_conversion(img_height - box[1][0], img_height)}")
+                    print(f"Point 3  x:{self.x_conversion(img_width - box[2][1],img_width)}  y:{self.y_conversion(img_height - box[2][0], img_height)}")
+                    print(f"Point 4  x:{self.x_conversion(img_width - box[3][1],img_width)}  y:{self.y_conversion(img_height - box[3][0], img_height)}")
+                    
+                    # box = np.int0(box)
+                    # cv2.drawContours(self.frame,[box],0,(0,0,255),2)
+                    self.tuboDetected = True
         
         return None
         
@@ -61,8 +81,8 @@ class tuboDetection:
         
 
         #orange mask
-        lower_orange =  [ 0, 122, 148]
-        upper_orange = [ 51, 250, 255]
+        lower_orange =   [ 0, 66, 193]
+        upper_orange =  [ 38, 231, 255]
 
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         orange_mask = self.get_mask(hsv, lower_orange, upper_orange)
@@ -85,18 +105,16 @@ class tuboDetection:
         return self.get_square_area(orange_mask), orange_result
     
     def detect_sensors(self):
-        sensor = 0
-        
         
         success, self.frame = self.capture.read()
         if success == False:
             raise ConnectionError
-
+        
         medidas_tubo , orange_image = self.getSquares(self.frame)
         
         
-        #cv2.imshow('orange', orange_image)
-        #cv2.imshow('Camera',self.frame)
+        cv2.imshow('orange', orange_image)
+        cv2.imshow('Camera',self.frame)
 
         
     
@@ -109,18 +127,13 @@ class tuboDetection:
 
 if __name__ == "__main__":
     detecting = tuboDetection()
-    fimdoTubo = False
-   # buz = Buzzer(22)
 
-    while  not detecting.tuboDetected :
+    while  not detecting.tuboDetected:
         detecting.detect_sensors()
-        
-    #tubo medidas = [x,y,h,w]
 
-            
+          
     detecting.capture.release()
     cv2.destroyAllWindows()
 
     cv2.waitKey(0)
-    
     
